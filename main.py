44c1c02331143d6c66e1abe9b2d41df7871bde0b
Mouaad <mouaad_ben_allal@hotmail.com>
2019-11-15 12:41:13 +0100

    added wifi connection

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
 from machine import Pin, I2C, Timer
-import  time
+import time
 from display.SSD1306 import SSD1306
 from display.SSD1306 import SH1106_I2C
+import socket
+from network import WLAN
+
+wifi_ssid = 'mobile hotspot'
+
+wifi_password = 'password'
+
+wlan = WLAN(mode=WLAN.STA)
+nets = wlan.scan()  # Scan all SSID networks
+print('WLAN init')
+for net in nets:
+    print(net)
+    if net.ssid == wifi_ssid:
+        wlan.connect(net.ssid, auth=(
+            net.sec, wifi_password), timeout=5000)
+        while not wlan.isconnected():
+            machine.idle()  # Save power while waiting
+        print('WLAN connection succeeded!')
+        break
+
+# Get address information of site
+url = 'towel.blinkenlights.nl'
+addr_info = socket.getaddrinfo(url, 23)
+# Get the IP and port
+addr = addr_info[0][-1]
+
+# Connect to it via socket
+s = socket.socket()
+s.connect(addr)
+
+# Print content/animation in console
+# Use Ctrl-C to interrupt
+while True:
+    data = s.recv(500)
+    print(str(data, 'utf8'), end='')
 
 
 i2c = I2C(0)                         # create on bus 0
-i2c = I2C(0, I2C.MASTER)  
-i2c = I2C(0, pins=('P9','P10'))     # create and use non-default PIN assignments (P10=SDA, P11=SCL)
+i2c = I2C(0, I2C.MASTER)
+# create and use non-default PIN assignments (P10=SDA, P11=SCL)
+i2c = I2C(0, pins=('P9', 'P10'))
 i2c.init(I2C.MASTER, baudrate=20000)
 
 oled = SH1106_I2C(128, 64, i2c)
@@ -18,7 +54,6 @@ while True:
     trigger(0)
 
     chrono = Timer.Chrono()
-
 
     chrono.reset()
 
@@ -45,4 +80,3 @@ while True:
         oled.text("Distance {:.0f} cm".format(distance), 0, 20)
         oled.show()
     time.sleep(1)
-
